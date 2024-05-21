f45c368fe09cbc9181511e5c8f3680971bfc7947
ittybittyfishy <oliviafishbowl@gmail.com>
2024-05-21 15:10:14 -0700

    connected to database

--- BookNook/app/src/main/java/com/example/booknook/HomePage.kt
+++ BookNook/app/src/main/java/com/example/booknook/HomePage.kt
@@ -3,19 +3,39 @@ package com.example.booknook
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.TextView
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.firestore.FirebaseFirestore
 
 class HomePage : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home_page)
+        val auth = FirebaseAuth.getInstance()
+        val db = FirebaseFirestore.getInstance()
 
-        // Retrieve the username from the intent extras
-        val username = intent.getStringExtra("username")
+        // get users UID
+        val userId = auth.currentUser?.uid
 
         // Find the TextView in the layout
         val loggedInTextView: TextView = findViewById(R.id.loggedInTextView)
 
-        // Set the text to display "logged in as username"
-        loggedInTextView.text = "Logged in as\n $username"
+        // if user is logged in, fetch username from firebase
+        userId?.let { uid ->
+            db.collection("users").document(uid).get().addOnSuccessListener { document ->
+                if (document.exists()) {
+                    val username = document.getString("username")
+
+                    // Set the text to display "logged in as username"
+                    loggedInTextView.text = "Logged in as\n$username"
+                } else {
+                    // Handle case where user document does not exist
+                    loggedInTextView.text = "Username not found"
+                }
+            }
+                .addOnFailureListener { exception ->
+                    // Handle errors
+                    loggedInTextView.text = "Error: ${exception.message}"
+                }
+        }
     }
 }
