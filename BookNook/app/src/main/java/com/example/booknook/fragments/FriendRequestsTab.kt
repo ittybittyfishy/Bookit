6de514fc0d12e57b836d8eae89f78ddca30435f8
Veronica Nguyen <veronicanguyen78@gmail.com>
2024-07-26 01:26:56 -0700

    Created tabs & fragments for Friends section
    Added Friends, Requests, and Blocked tabs

--- BookNook/app/src/main/java/com/example/booknook/fragments/FriendRequestsTab.kt
+++ BookNook/app/src/main/java/com/example/booknook/fragments/FriendRequestsTab.kt
@@ -8,9 +8,10 @@ import android.view.ViewGroup
 import android.widget.Button
 import com.example.booknook.MainActivity
 import com.example.booknook.R
+import com.google.firebase.auth.FirebaseAuth
 
-class FriendsFragment : Fragment() {
-
+class FriendRequestsTab : Fragment() {
+    private lateinit var friendsButton: Button
     private lateinit var requestsButton: Button
     private lateinit var blockedButton: Button
 
@@ -19,21 +20,21 @@ class FriendsFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_friends, container, false)
+        return inflater.inflate(R.layout.fragment_friend_requests_tab, container, false)
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
         // Initialize buttons
-        requestsButton = view.findViewById(R.id.requests_button)
+        friendsButton = view.findViewById(R.id.friends_button)
         blockedButton = view.findViewById(R.id.blocked_button)
 
         // Set listeners
-        requestsButton.setOnClickListener {
+        friendsButton.setOnClickListener {
             // Handle account button click
-            val requestsFragment = FriendRequestsTab()
-            (activity as MainActivity).replaceFragment(requestsFragment, "Friends")
+            val friendsFragment = FriendsFragment()
+            (activity as MainActivity).replaceFragment(friendsFragment, "Friends")
         }
 
         blockedButton.setOnClickListener {
