108e3235c221f11684b05664a6767843a90d5955
Veronica Nguyen <veronicanguyen78@gmail.com>
2024-07-27 01:26:46 -0700

    Added search bar for friend requests
    To-do: Display user's profile after looking up their username

--- BookNook/app/src/main/java/com/example/booknook/fragments/FriendsFragment.kt
+++ BookNook/app/src/main/java/com/example/booknook/fragments/FriendsFragment.kt
@@ -6,6 +6,8 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
 import com.example.booknook.MainActivity
 import com.example.booknook.R
 
@@ -13,6 +15,8 @@ class FriendsFragment : Fragment() {
 
     private lateinit var requestsButton: Button
     private lateinit var blockedButton: Button
+    private lateinit var searchButton: Button
+    private lateinit var searchBar: EditText
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -28,6 +32,8 @@ class FriendsFragment : Fragment() {
         // Initialize buttons
         requestsButton = view.findViewById(R.id.requests_button)
         blockedButton = view.findViewById(R.id.blocked_button)
+        searchButton = view.findViewById(R.id.search_friend_button)
+        searchBar = view.findViewById(R.id.search_friend_bar)
 
         // Set listeners
         requestsButton.setOnClickListener {
@@ -41,5 +47,18 @@ class FriendsFragment : Fragment() {
             val blockedFragment = BlockedFriendsTab()
             (activity as MainActivity).replaceFragment(blockedFragment, "Friends")
         }
+
+        searchButton.setOnClickListener {
+            val query = searchBar.text.toString().trim()
+            if (query.isNotEmpty()) {
+                searchUser(query)
+            } else {
+                Toast.makeText(activity, "Please enter a username", Toast.LENGTH_SHORT).show()
+            }
+        }
+    }
+
+    private fun searchUser(username: String) {
+        // To-do: Display user's profile after looking up their username
     }
 }
