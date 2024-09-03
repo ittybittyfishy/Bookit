29e032b9420c71a194b3e712a77f168e297e2f36
dji03120 <dji03120@naver.com>
2024-09-03 00:50:21 -0700

    darkmode_working

--- BookNook/app/src/main/java/com/example/booknook/fragments/SettingsFragment.kt
+++ BookNook/app/src/main/java/com/example/booknook/fragments/SettingsFragment.kt
@@ -10,16 +10,20 @@ import android.widget.Button
 import com.example.booknook.Login
 import com.google.firebase.auth.FirebaseAuth
 import android.content.Intent
+import android.widget.Switch
 import android.widget.Toast
+import androidx.appcompat.app.AppCompatDelegate
 import com.example.booknook.Account
 import com.example.booknook.MainActivity
 import com.example.booknook.Register
+import android.content.res.Configuration
 
 class SettingsFragment : Fragment() {
 
     lateinit var accountButton: Button
     lateinit var notificationButton: Button
     lateinit var signOutButton: Button
+    lateinit var darkModeSwitch: Switch
     lateinit var auth: FirebaseAuth
 
 
@@ -38,6 +42,7 @@ class SettingsFragment : Fragment() {
         accountButton = view.findViewById(R.id.account_button)
         notificationButton = view.findViewById(R.id.notification_button)
         signOutButton = view.findViewById(R.id.sign_out)
+        darkModeSwitch = view.findViewById(R.id.darkModeSwitch)
         auth = FirebaseAuth.getInstance()
 
         // Set listeners
@@ -53,7 +58,6 @@ class SettingsFragment : Fragment() {
             (activity as MainActivity).replaceFragment(notificationFragment, "Notifications")
         }
 
-
         signOutButton.setOnClickListener {
             // Handle sign out button click
             auth.signOut()
@@ -62,5 +66,21 @@ class SettingsFragment : Fragment() {
             Toast.makeText(requireContext(), "You have been logged out", Toast.LENGTH_SHORT).show()
             activity?.finish()
         }
+
+        darkModeSwitch.isChecked = isDarkThemeOn()
+
+        // Handle the switch toggle action
+        darkModeSwitch.setOnCheckedChangeListener { _, isChecked ->
+            if (isChecked) {
+                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
+            } else {
+                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
+            }
+        }
+
+    }
+
+    private fun isDarkThemeOn(): Boolean {
+        return (resources.configuration.uiMode and Configuration.UI_MODE_NIGHT_MASK) == Configuration.UI_MODE_NIGHT_YES
     }
 }
